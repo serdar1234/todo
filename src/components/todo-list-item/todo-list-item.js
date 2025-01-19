08da954a037a957db266cf8cfe9ead29dafdf802
Serdar Geldyev <serdarabad@gmail.com>
2025-01-19 15:18:19 +0300

    turned class component into fn

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,41 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './todo-list-item.css';
 
-export default class ToDoListItem extends Component {
-  onLabelClick() {
-    this.setState( ({done}) => {
-      return {done: !done}
-    } )
-  }
+function ToDoListItem(props) {
+  const { label, done, important,
+    onDeleted, onDone, onImportant
+    } = props;
+  let classNames = "todo-list-item";
 
-  render() {
-    const { label, onDeleted, done, important,
-      onDone, onImportant
-     } = this.props;
-    let classNames = "todo-list-item";
+  if (done)       classNames += " done";
+  if (important)  classNames += " important"
 
-    if (done)       classNames += " done";
-    if (important)  classNames += " important"
-
-    return (
-      <span className= { classNames }>
-        <span
-          className="todo-list-item-label"
-          onClick={ onDone}>
-          {label}
-        </span>
-  
-        <button type="button"
-          onClick={ onImportant } className="btn btn-outline-success btn-sm float-right">
-          <i className="fa fa-exclamation" />
-        </button>
-  
-        <button type="button"
-          onClick={ onDeleted } className="btn btn-outline-danger btn-sm float-right">
-          <i className="fa fa-trash-o" />
-        </button>
+  return (
+    <span className= { classNames }>
+      <span
+        className="todo-list-item-label"
+        onClick={ onDone}>
+        {label}
       </span>
-    );
-  }
+
+      <button type="button"
+        onClick={ onImportant } className="btn btn-outline-success btn-sm float-right">
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button type="button"
+        onClick={ onDeleted } className="btn btn-outline-danger btn-sm float-right">
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
 }
+
+export default ToDoListItem;
